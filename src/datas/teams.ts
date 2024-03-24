df527d9e0b974938ab7ef8ff620e183da7d2f8cd
ronaldj220 <ronjuliostagjhs21@gmail.com>
2024-03-24 16:20:17 +0700

    fixing: styling about

---
+++ src/datas/teams.ts
@@ -0,0 +1,22 @@
+// Import Gambar terkait teams
+import img1 from "../../public/assets/img/01.webp";
+import img2 from "../../public/assets/img/15.webp";
+import img3 from "../../public/assets/img/18.webp";
+
+export const teams = [
+  {
+    img: img2,
+    name: "Andi Pratama",
+    role: "Lead Developer",
+  },
+  {
+    img: img1,
+    name: "Siti Rahma",
+    role: "UI/UX Designer",
+  },
+  {
+    img: img3,
+    name: "Budi Santoso",
+    role: "Project Manager",
+  },
+];
