35c276bb7dd3b0469dbf80b14b502d5233084237
ronaldj220 <ronjuliostagjhs21@gmail.com>
2024-03-25 06:28:19 +0700

    feature: adding Teams Component

--- src/datas/teams.ts
+++ src/datas/teams.ts
@@ -1,22 +1,10 @@
-// Import Gambar terkait teams
-import img1 from "../../public/assets/img/01.webp";
-import img2 from "../../public/assets/img/15.webp";
-import img3 from "../../public/assets/img/18.webp";
+import axios from "axios";
 
-export const teams = [
-  {
-    img: img2,
-    name: "Andi Pratama",
-    role: "Lead Developer",
-  },
-  {
-    img: img1,
-    name: "Siti Rahma",
-    role: "UI/UX Designer",
-  },
-  {
-    img: img3,
-    name: "Budi Santoso",
-    role: "Project Manager",
-  },
-];
+export const getTeamData = async () => {
+  try {
+    const response = await axios.get("https://randomuser.me/api/?results=3");
+    return response.data.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
